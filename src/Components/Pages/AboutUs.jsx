389c022398060b35ff38b3a6ff269b61fd590c4e
Diego <alfiandrodiego@gmail.com>
2022-11-29 16:59:50 +0700

    Kurang bagian about pesan dan kesan dari vanya, dino, ihza, brucel, arif . awwkwkwk

--- src/Components/Pages/AboutUs.jsx
+++ src/Components/Pages/AboutUs.jsx
@@ -2,44 +2,49 @@ import React from 'react'
 import Navbar from '../Navbar'
 import './Landing.css'
 import boy from './boy.svg'
-import girl from './girl.svg'
+import brucel from './foto_brucel.jpg'
+import vanya from './foto_vanya.jpg'
+import diego from './foto_diego.jpeg'
+import himawan from './foto_himawan.jpeg'
+import arif from './foto_dino.png'
+import dino from './foto_arif.jpg'
 import doctor from "./doctor.svg"
 
 const AboutUs = () => {
     const about = [
         {
-          gambar : `${boy}`,
+          gambar : `${diego}`,
           nama : "Diego Alfiandro",
           title : "Front-End Dev",
-          pesan : "Not Bad"
+          pesan : "Belajar web developer di Skilvul terutama dibidang Front-End Web Dev menyenangkan dan tidak membosankan"
         },
         {
-            gambar : `${boy}`,
-            nama : "Diego Alfiandro",
+            gambar : `${himawan}`,
+            nama : "Himawan Ihza Pradana",
             title : "Front-End Dev & Leader",
             pesan : "Not Bad"
           },
           {
-            gambar : `${boy}`,
-            nama : "Diego Alfiandro",
+            gambar : `${brucel}`,
+            nama : "Brucel Duta Samudra",
             title : "Front-End Dev",
             pesan : "Not Bad"
           },
           {
-            gambar : `${boy}`,
-            nama : "Diego Alfiandro",
-            title : "Front-End Dev",
+            gambar : `${arif}`,
+            nama : "Arif Setiawan",
+            title : "Back-End Dev",
             pesan : "Not Bad"
           },
           {
-            gambar : `${girl}`,
-            nama : "Diego Alfiandro",
-            title : "Front-End Dev",
+            gambar : `${vanya}`,
+            nama : "Vanya Mayazura P.",
+            title : "Back-End Dev",
             pesan : "Not Bad"
           },
           {
-            gambar : `${boy}`,
-            nama : "Diego Alfiandro",
+            gambar : `${dino}`,
+            nama : "Zuhri Agusdino",
             title : "Front-End Dev",
             pesan : "Not Bad"
           },
@@ -48,34 +53,34 @@ const AboutUs = () => {
     const fitur = [
       {
         fitur : "Tes Kesehatan Mental",
-        deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis explicabo nesciunt quae aspernatur iure minima reiciendis quibusdam totam repellendus porro. Ipsum hic delectus quibusdam facilis cum sint! Tenetur, quidem quos?",
+        deskripsi : "Tes kesehatan mental merupakan suatu kesehatan mental pada dasarnya adalah untuk mendeteksi lebih cepat atau menentukan risiko seseorang untuk mengalami gangguan mental, seperti gangguan kecemasan, depresi, gangguan bipolar,",
         kategori : "Ringan, Sedang, Berat"
       },
       {
         fitur : "Tes Tingkat Kesepian",
-        deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis explicabo nesciunt quae aspernatur iure minima reiciendis quibusdam totam repellendus porro. Ipsum hic delectus quibusdam facilis cum sint! Tenetur, quidem quos?",
+        deskripsi : "Rasa kesepian yang tinggi dikhawatirkan dapat memicu masalah terhadap kesehatan mental kamu.Rasa sepi, sedih, cemas, dan khawatir akan menghantui saat dukungan sosial yang diperlukan tak lagi bisa dirasakan oleh diri.",
         kategori : "Ringan, Sedang, Berat"
       },
       {
         fitur : "Tes Tingkat Overthinking",
-        deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis explicabo nesciunt quae aspernatur iure minima reiciendis quibusdam totam repellendus porro. Ipsum hic delectus quibusdam facilis cum sint! Tenetur, quidem quos?",
+        deskripsi : "Overthinking membuat seseorang terlalu banyak merenungi masa lalu, mengkhawatirkan masa depan, dan memikirkan berbagai kemungkinan yang belum tentu akan benar-benar terjadi.",
         kategori : "Ringan, Sedang, Berat"
       },
       {
-        fitur : "Informasi Articel",
-        deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis explicabo nesciunt quae aspernatur iure minima reiciendis quibusdam totam repellendus porro. Ipsum hic delectus quibusdam facilis cum sint! Tenetur, quidem quos?",
+        fitur : "Fitur Informasi Articel",
+        deskripsi : "Pada bagian ini berfungsi untuk menampilkan beberapa articel terbaru terkait dengan pentingnya kesehatan mental dan beberapa literatur mengenai kesehatan mental yang umum dialami remaja",
         kategori : ""
       },
       {
-        fitur : "Daftar Psikolog",
-        deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis explicabo nesciunt quae aspernatur iure minima reiciendis quibusdam totam repellendus porro. Ipsum hic delectus quibusdam facilis cum sint! Tenetur, quidem quos?",
+        fitur : "Fitur Daftar Psikolog",
+        deskripsi : "Pada fitur ini berisi daftar psikolog beserta data diri mulai dari Nama, Jenis kelamin, Alamat dan Kontak yang dapat dihubungi. User dapat melakukan konsultasi melalui kontak yang tersedia",
         kategori : ""
       },
     ]
     return (
         <>
             <Navbar/>
-            <div className="pt-6 pb-16 bg-sky-100 mt-10 w-14/12 ml-16 mr-14 rounded-lg ">
+            <div className="pt-6 pb-16 bg-sky-100 mt-10 w-14/12 ml-10 mr-10 rounded-lg ">
               <div className="flex lg:flex-row mt-20 gap-20 items-center md : flex-col">
                   <div className='basis-auto ml-20 mr-20'>
                       <h1 className='text-4xl font-extrabold text-base' style={{color : '#1B97CC'}}>Sapasikolog</h1><br />
@@ -87,9 +92,9 @@ const AboutUs = () => {
                   </div>
               </div>
             </div>
-            <div className="pt-6 pb-16 bg-sky-100 w-14/12 mt-10 ml-16 mr-14 rounded-lg">
+            <div className="pt-6 pb-16 bg-sky-100 w-14/12 mt-10 ml-10 mr-10 rounded-lg">
               <h1 className='text-center font-extrabold text-5xl' style={{color : '#1B97CC'}}>Fitur</h1>
-              <div className="grid gap-3 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 ml-20 mr-20 mt-10 mb-20">
+              <div className="grid gap-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols- ml-20 mr-20 mt-10 mb-20">
                       {fitur.map((items, key) => (
                           <div className="w-full rounded-lg bg-sky-200 hover:bg-sky-400 hover:shadow-lg mt-10" key={key} style={{}}>
                               <div className="p-4">
@@ -102,16 +107,16 @@ const AboutUs = () => {
                       ))}
               </div>
         </div>
-            <div className="pt-6 pb-16 bg-sky-100 mt-10 w-14/12 ml-16 mr-14 rounded-lg mb-5 ">
+            <div className="pt-6 pb-16 bg-sky-100 mt-10 ml-10 mr-10 rounded-lg mb-5 ">
             <h1 className='text-4xl font-extrabold text-center' style={{color : '#1B97CC'}}>TEAM</h1><br />
-              <div className="grid gap-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 ml-20 mr-20 mt-10 mb-20">
+              <div className="grid gap-2 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 ml-20 mt-10 mb-20">
                       {about.map((items, key) => (
-                          <div className="w-4/5 rounded-lg bg-sky-200 lg:max-w-sm hover:bg-sky-400 hover:shadow-lg mt-10" key={key} style={{}}>
+                          <div className="w-4/5 rounded-lg outline outline-3 shadow-md outline-sky-400 lg:max-w-sm hover:bg-sky-400 hover:shadow-lg mt-10" key={key} style={{}}>
                               <div className="p-4">
-                                <img className=' w-11/12 hover:w-full' src={items.gambar} alt="belom ada" />
-                                  <h3 className="text-xl text-center font-bold text-sky-600 mt-5">{items.nama}</h3>
-                                  <h2 className="text-2xl text-sky-600 text-center mt-3">{items.title}</h2>
-                                  <p className="mb-2 text-center leading-normal text-black-700 text-base mb-2 mt-10">
+                                <img className='rounded-full' src={items.gambar} alt="belom ada" />
+                                  <h3 className="text-xl font-bold text-center mt-5" style={{color : '#1B97CC'}}>{items.nama}</h3>
+                                  <h2 className="text-xl text-sky-600 text-center mt-3">{items.title}</h2>
+                                  <p className="mb-2 text-center leading-normal italic text-base mb-2 mt-10">
                                      " {items.pesan} "</p>
                               </div>
                           </div>
